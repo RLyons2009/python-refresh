195949501a2ab281202b7359e74a17f77ce20c07
Rhys <735655@student.bedford.ac.uk>
2026-09-25 13:49:29 +0100

    pygame 2

--- LightsPygame.py
+++ LightsPygame.py
@@ -14,30 +14,30 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    start_time = 5000
     elapsed = pygame.time.get_ticks() - start_time
     screen.fill("black")
     pygame.draw.circle(screen, "grey20", (150,75),50) #red
     pygame.draw.circle(screen, "grey20", (150,225),50) #amber
     pygame.draw.circle(screen, "grey20", (150,375),50) #green
-    if elapsed >= 5000:
-        if light == "Red":
-            light = "Green"
-        elif light == "Green":
-            light = "Amber"
-        elif light == "Amber":
-            light = "Red"
-
-        start_time = 5000
-
-    if light == "Red":
-        pygame.draw.circle(screen, "red", (150,75),50) #red
-
-    elif light == "Amber":
-            pygame.draw.circle(screen, "amber", (150,225),50) #red
-
-    else:
-            pygame.draw.circle(screen, "green", (150,375),50) #red
     
+    if light == "Red":
+        pygame.draw.circle(screen, "red", (150,75),50) 
+        light = "rAmber"
+        pygame.time.wait(1000)
+    elif light == "rAmber":
+        pygame.draw.circle(screen, "orange", (150,225),50) 
+        light = "Green"
+        pygame.time.wait(2000)
+    elif light == "Green":
+        pygame.draw.circle(screen, "green", (150,375),50) 
+        light = "gAmber"
+        pygame.time.wait(1000)
+    elif light == "gAmber":
+        pygame.draw.circle(screen, "orange", (150,225),50) 
+        light = "Red"
+        pygame.time.wait(2000)
+
     pygame.display.flip()
 
 pygame.quit()
